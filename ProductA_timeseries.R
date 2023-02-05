34fb7cb1a7e61405006495dc87cb996581331962
NishalThampan <nishalthampan@gmail.com>
2023-02-05 21:14:37 +0000

    Modifications of comments

--- ProductA_timeseries.R
+++ ProductA_timeseries.R
@@ -12,14 +12,18 @@ setwd("D:/Demand_Forecasting")
 
 productA <- read.csv("ProductA.csv") # Read sales quantity from csv file
 
+# Splitting of time-series data into training and testing set
+
 split_point <- floor(0.8 * nrow(productA))
 productA_training <- productA[1:split_point,]
 productA_testing <- productA[(split_point + 1):nrow(productA), ]
 
+# Defining a time series object using the tarining data set 
+
 productA_ts <- ts(productA_training$Quantity, start = 1, frequency = 7)
 autoplot(productA_ts) + ggtitle("Product A sales") + labs(x = 'time', y = 'Sales')
 
-productA_clean <- SlidingWindow("mean",productA_training$Quantity,3,1)
+productA_clean <- SlidingWindow("mean",productA_training$Quantity,3,1) # Cleaning of data set using sliding window
 productA_ts <- ts(productA_clean, start = 1, frequency = 7)
 
 autoplot(productA_ts) + ggtitle("Product A sales") + labs(x = 'time', y = 'Sales')
@@ -32,7 +36,11 @@ d_productA_ts <- diff(productA_ts)
 ggAcf(d_productA_ts) + ggtitle("ACF")
 ggPacf(d_productA_ts) + ggtitle("PACF")
 
+# Decomposition of time series object
+
 ts_decompose(productA_ts, type = "additive", showline = TRUE)
+
+# Tests for stationarity
 
 adf.test(productA_ts)
 adf.test(productA_ts, k=1)
